d153bbf1f2abc9ba1641b75dfb648674a44e189b
KeyT9999 <111619268+KeyT9999@users.noreply.github.com>
2025-07-01 09:42:08 +0700

    Add .next/static/chunks/src_app_page_tsx_191f5e9e._.js 1751337727674

---
+++ .next/static/chunks/src_app_page_tsx_191f5e9e._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_22439a28._.js",
+  "static/chunks/node_modules_6019879d._.js"
+],
+    source: "dynamic"
+});
